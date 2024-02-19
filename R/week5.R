32e5dd1a1c4c9dfb5b93dd650793a5c0d03ea538
Amanda Jensen <jens2482@umn.edu>
2024-02-19 14:31:16 -0600

    Changes to code from class lecture

--- R/week5.R
+++ R/week5.R
@@ -5,20 +5,22 @@ library(tidyverse)
 # Data Import
  Adata_tbl<- read_delim ('../data/Aparticipants.dat', delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
  Anotes_tbl<- read_csv ('../data/Anotes.csv')
- Bdata_tbl<- read_delim ('../data/Bparticipants.dat', delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10)))
+ Bdata_tbl<- read_delim ('../data/Bparticipants.dat', delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10))) #readtsv also option
  Bnotes_tbl<- read_delim ('../data/Bnotes.txt', delim = "\t")
  
  # Data Cleaning
  Aclean_tbl <- Adata_tbl %>%
-   separate(qs, into = c("q1", "q2", "q3", "q4","q5"), sep = "-") %>%
+   separate(qs, into = c(paste0("q", 1:5))) %>%
    mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
    mutate(across(starts_with("q"), as.integer)) %>%
-   left_join(Anotes_tbl, by = c("casenum" = "parnum")) %>%
+   # mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")),
+   # (across(starts_with("q"), as.integer)) %>% #this line and previous are alternate way to code two mutates in a row (second row has implied mutate by putting column between mutates)
+   inner_join(Anotes_tbl, by = "parnum") %>%
    filter(is.na(notes))
  ABclean_tbl <- Bdata_tbl %>%
    mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
    mutate(across(starts_with("q"), as.integer)) %>%
-   left_join(Bnotes_tbl, by = c("casenum" = "parnum")) %>%
+   inner_join(Bnotes_tbl, by = "parnum") %>%
    filter(is.na(notes)) %>%
    bind_rows(Aclean_tbl, .id = "lab") %>%
    select(-notes)
