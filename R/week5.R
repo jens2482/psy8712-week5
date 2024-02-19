9aa4466ea2800ecc243962a02cb2dbe5d9e6d37b
Amanda Jensen <jens2482@umn.edu>
2024-02-19 09:42:55 -0600

    Completed - come back to line 23

--- R/week5.R
+++ R/week5.R
@@ -15,3 +15,11 @@ library(tidyverse)
    mutate(across(starts_with("q"), as.integer)) %>%
    left_join(Anotes_tbl, by = c("casenum" = "parnum")) %>%
    filter(is.na(notes))
+ ABclean_tbl <- Bdata_tbl %>%
+   mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>%
+   mutate(across(starts_with("q"), as.integer)) %>%
+   left_join(Bnotes_tbl, by = c("casenum" = "parnum")) %>%
+   filter(is.na(notes)) %>%
+   bind_rows(mutate(Aclean_tbl, lab = "A")) %>% #I cannot for the life of my figure out how to do this. I've spent hours trying to figure out how to get the data from lab B labeled as such, but it always duplicates the rows in the dataset. So for now, data from labs A and B are technically still differentiated, with lab A labeled as "A" and lab B containing NA. 
+   select(-notes)
+ 
